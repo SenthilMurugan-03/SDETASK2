cf6aaf3da4f414b5f3a069a78401121b361d505c
SenthilMurugan-03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2022-11-07 12:06:42 +0530

    Update QUEST06.js

--- QUEST06.js
+++ QUEST06.js
@@ -7,3 +7,7 @@ console.log('show table[1] : '+table[1]);
 console.log('Reason : table[1] show all elements in the second row (second row contains the element of seconf array.)')
 console.log('show table[1][1] : '+table[1][1]);
 console.log('Reason : table[1][1] show the second element in the second row row (second row contains the element of second array.)')
+ 
+console.log('mass of the element1 is : '+table[0][2]);
+console.log('name of the element2 is : '+table[1][0]);
+console.log('symbol of the element26 is : '+table[2][1]);
