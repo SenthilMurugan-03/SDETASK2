89f5635a6fcb129870cc459d3f24269b40346e19
SenthilMurugan-03 <115228001+SenthilMurugan-03@users.noreply.github.com>
2022-11-07 12:02:25 +0530

    Create QUEST06.js

---
+++ QUEST06.js
@@ -0,0 +1,9 @@
+const element1 = ['hydrogen', 'H', 1.008];
+const element2 = ['helium', 'He', 4.003] ;
+const element26 = ['iron', 'Fe', 55.85];
+const table=[element1,element2,element26];
+console.log(table);
+console.log('show table[1] : '+table[1]);
+console.log('Reason : table[1] show all elements in the second row (second row contains the element of seconf array.)')
+console.log('show table[1][1] : '+table[1][1]);
+console.log('Reason : table[1][1] show the second element in the second row row (second row contains the element of second array.)')
